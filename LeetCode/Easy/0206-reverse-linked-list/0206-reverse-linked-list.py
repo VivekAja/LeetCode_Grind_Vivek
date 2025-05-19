79ac20320859eebcbbac73bea9283e60293958d5
Vivek Ajarekar <108683998+VivekAja@users.noreply.github.com>
2025-05-20 01:48:53 +0200

    Update 0206-reverse-linked-list.py

--- LeetCode/Easy/0206-reverse-linked-list/0206-reverse-linked-list.py
+++ LeetCode/Easy/0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -31,24 +31,6 @@ Recursive:
 # TC: O(n) 
 # Space: O(n) 
     def reverseList(self, head):
-        """
-        :type head: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        
-        curr = head
-        prev_nonexistantNode = None
-
-        while curr:
-            upcomingNode = curr.next
-
-            curr.next = prev_nonexistantNode
-
-            prev_nonexistantNode = curr
-
-            curr = upcomingNode
-        
-        return prev_nonexistantNode
-"""
         if not head or not head.next:
             return head
 

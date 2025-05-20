9a9f210ca758eb2c5c5726fb02772827cd3dd470
Vivek Ajarekar <108683998+VivekAja@users.noreply.github.com>
2025-05-20 18:26:46 +0200

    Update 1161-maximum-level-sum-of-a-binary-tree.py

--- LeetCode/Medium/1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.py
+++ LeetCode/Medium/1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.py
@@ -1,3 +1,7 @@
+#DFS 
+#TC: O(n)
+#SC: O(h)
+
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
@@ -20,7 +24,6 @@ class Solution(object):
                 res.append(node.val)
             else:
                 res[level] += node.val
-            #res.append(temp)
             dfs(node.left, level + 1)
             dfs(node.right, level + 1)
 
@@ -28,3 +31,39 @@ class Solution(object):
         larg = max(res)
         l = res.index(larg)
         return l+1
+
+#BFS 
+#TC: O(n)
+#SC: O(h)
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxLevelSum(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return []
+        res = 0
+        result= []
+        queue = deque([root])
+
+        while queue:
+            level = len(queue)
+            temp = 0
+            for i in range(level):
+                node = queue.popleft()
+                #if i == level - 1:
+                temp += node.val
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            #res = max(res, temp)
+            result.append(temp)
+
+        larg = max(result)
+        l = result.index(larg)
+        return l+1
+        

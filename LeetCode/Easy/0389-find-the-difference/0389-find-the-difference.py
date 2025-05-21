c317b268922841bf02350816f74c35417b9bd9dd
Vivek Ajarekar <108683998+VivekAja@users.noreply.github.com>
2025-05-21 12:54:02 +0200

    Update 0389-find-the-difference.py

--- LeetCode/Easy/0389-find-the-difference/0389-find-the-difference.py
+++ LeetCode/Easy/0389-find-the-difference/0389-find-the-difference.py
@@ -1,7 +1,29 @@
-
+#Optimal
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
         result = 0
         for char in s + t:
             result ^= ord(char)
         return chr(result)
+
+
+=========================================================
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        seen = dict()
+
+        for c in s:
+            if c not in seen:
+                seen[c] = 0
+            seen[c] += 1
+
+        print(seen)
+
+        for c in t:
+            if c not in seen:
+                return c
+
+            if seen[c] == 0:
+                return c
+
+            seen[c] -= 1

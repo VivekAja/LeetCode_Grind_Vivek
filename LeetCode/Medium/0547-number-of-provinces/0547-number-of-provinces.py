57164b55a9ba03cb955a7f9f9b8412b2c3499922
Vivek Ajarekar <108683998+VivekAja@users.noreply.github.com>
2025-05-23 11:36:56 +0200

    Update 0547-number-of-provinces.py

--- LeetCode/Medium/0547-number-of-provinces/0547-number-of-provinces.py
+++ LeetCode/Medium/0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,3 +1,8 @@
+#BFS
+
+#Time Complexity: O(n²)
+#Space Complexity: O(n)
+
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
         def dfs(city):
@@ -16,3 +21,35 @@ class Solution:
                 provinces += 1
 
         return provinces
+
+
+============================================================================
+#BFS
+
+#Time Complexity: O(n²)
+#Space Complexity: O(n)
+
+
+
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        n = len(isConnected)
+        visited = set()
+        provinces = 0
+        def bfs(start):
+            queue = deque([start])
+            visited.add(start)
+            while queue:
+                fromnode = queue.popleft()
+                print(fromnode)
+                for tonode in range(n):
+                    if isConnected[fromnode][tonode] and tonode not in visited:
+                        visited.add(tonode)
+                        queue.append(tonode)
+
+        for i in range(n):
+            if i not in visited:
+                bfs(i)
+                provinces += 1
+        return provinces
+        

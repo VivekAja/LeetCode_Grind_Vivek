b116a3d7b9e4420e04025fa54ac68f36fc9814b6
Vivek Ajarekar <108683998+VivekAja@users.noreply.github.com>
2026-05-26 09:29:38 +0530

    3120-count-the-number-of-special-characters-i 05-26-2026

--- LeetCode/Easy/3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.py
+++ LeetCode/Easy/3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.py
@@ -1,12 +1,13 @@
 from collections import Counter
 class Solution:
     def numberOfSpecialChars(self, word: str) -> int:
-        char_counts = Counter(word)
+        #char_counts = Counter(word)
+        char = set(word)
         count = 0
         for i in range(26):
             lower = chr(97+i)
             upper = chr(65+i)
 
-            if char_counts [lower] > 0 and char_counts [upper] > 0:
+            if lower in char and upper in char:
                 count +=1
         return count

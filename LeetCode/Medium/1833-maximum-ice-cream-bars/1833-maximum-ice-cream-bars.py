26fc00e1e2dedfa55c7f74e59140e495a29f265a
Vivek Ajarekar <108683998+VivekAja@users.noreply.github.com>
2026-06-22 00:59:46 +0530

    1833-maximum-ice-cream-bars 06-22-2026

---
+++ LeetCode/Medium/1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxIceCream(self, costs: List[int], coins: int) -> int:
+        costs.sort()
+
+        for i, c in enumerate(costs):
+            if coins < c:
+                return i
+            coins -= c
+
+        return len(costs)
